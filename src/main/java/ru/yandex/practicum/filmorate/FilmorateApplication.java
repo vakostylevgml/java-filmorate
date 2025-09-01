fec2fc22fa5d4ce7072725a7f06b7b54abb9deef
Vladislav Kostylev <kostylev.v@gmail.com>
2025-09-01 22:07:32 +0300

    feat: todo for sprint 11

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -3,6 +3,19 @@ package ru.yandex.practicum.filmorate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Sprint 11 todo's
+ * @TODO filmstorage
+ * @TODO userstorage
+ *
+ * @TODO userservice: add, delete, findAll
+ * @TODO filmservice: like, mostpop
+ *
+ * @TODO inject
+ * @TODO full rest
+ * @TODO logging
+ */
+
 @SpringBootApplication
 public class FilmorateApplication {
 	public static void main(String[] args) {
