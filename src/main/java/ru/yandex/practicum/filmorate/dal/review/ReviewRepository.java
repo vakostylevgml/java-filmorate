5baeaa03c9021230730f3824b17384c4ae2343f1
Vladislav Kostylev <kostylev.v@gmail.com>
2025-10-17 15:02:20 +0300

    fix: refactor after code review

--- src/main/java/ru/yandex/practicum/filmorate/dal/review/ReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/review/ReviewRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ReviewStorage extends BaseRepository<Review> {
+public class ReviewRepository extends BaseRepository<Review> {
     private static final String INSERT_REVIEW = "INSERT INTO reviews (user_id, film_id, content, ispositive) " +
             " VALUES (?, ?, ?, ?)";
     private static final String UPDATE = "UPDATE reviews SET content = ?, ispositive = ? WHERE id = ?";
@@ -41,7 +41,7 @@ public class ReviewStorage extends BaseRepository<Review> {
     private static final String DELETE_LIKE_DISLIKE = "DELETE FROM review_likes_dislikes WHERE user_id = ? " +
             "AND review_id = ?";
 
-    public ReviewStorage(JdbcTemplate jdbc, RowMapper<Review> mapper, ResultSetExtractor<List<Review>> listExtractor) {
+    public ReviewRepository(JdbcTemplate jdbc, RowMapper<Review> mapper, ResultSetExtractor<List<Review>> listExtractor) {
         super(jdbc, mapper, listExtractor);
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dal.review.ReviewStorage;
+import ru.yandex.practicum.filmorate.dal.review.ReviewRepository;
 import ru.yandex.practicum.filmorate.dto.review.NewReviewRequest;
 import ru.yandex.practicum.filmorate.dto.review.ReviewDto;
 import ru.yandex.practicum.filmorate.dto.review.UpdatedReviewRequest;
@@ -22,25 +22,25 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
-    private final ReviewStorage reviewStorage;
+    private final ReviewRepository reviewRepository;
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
 
     public ReviewDto getReviewById(int id) {
-        return reviewStorage.getReviewById(id).map(ReviewMapper::mapToDto).orElseThrow(() ->
+        return reviewRepository.getReviewById(id).map(ReviewMapper::mapToDto).orElseThrow(() ->
                 new NotFoundException("Review with id + " + id + "not found"));
     }
 
     public List<ReviewDto> getReviewsByFilmId(int id) {
         Film film = filmStorage.getFilmById(id);
-        return reviewStorage.getReviewsByFilmId(id).stream().map(ReviewMapper::mapToDto).toList();
+        return reviewRepository.getReviewsByFilmId(id).stream().map(ReviewMapper::mapToDto).toList();
     }
 
     public ReviewDto createReview(NewReviewRequest request) {
         Film film = filmStorage.getFilmById(request.getFilmId());
         User user = userStorage.findUser(request.getUserId()).orElseThrow(() ->
                 new NotFoundException("Couldn't add review from unexisting user with id = " + request.getUserId()));
-        Review review = reviewStorage.createReview(ReviewMapper.mapToReview(request));
+        Review review = reviewRepository.createReview(ReviewMapper.mapToReview(request));
         return ReviewMapper.mapToDto(review);
     }
 
@@ -48,38 +48,38 @@ public class ReviewService {
         Film film = filmStorage.getFilmById(request.getFilmId());
         User user = userStorage.findUser(request.getUserId()).orElseThrow(() ->
                 new NotFoundException("Couldn't add review from unexisting user with id = " + request.getUserId()));
-        Review review = reviewStorage.createReview(ReviewMapper.mapToReview(request));
-        return ReviewMapper.mapToDto(reviewStorage.updateReview(ReviewMapper.mapToReview(request)));
+        Review review = reviewRepository.createReview(ReviewMapper.mapToReview(request));
+        return ReviewMapper.mapToDto(reviewRepository.updateReview(ReviewMapper.mapToReview(request)));
     }
 
     public void deleteReview(int id) {
-        Review review = reviewStorage.getReviewById(id).orElseThrow(() ->
+        Review review = reviewRepository.getReviewById(id).orElseThrow(() ->
                 new ValidationException("Couldn't delete unexisting review with id = " + id));
-        reviewStorage.deleteReview(id);
+        reviewRepository.deleteReview(id);
     }
 
     public void addLikeToReview(int userId, int reviewId) {
         User user = userStorage.findUser(userId).orElseThrow(() ->
                 new NotFoundException("Couldn't add review from unexisting user with id = " + userId));
-        Review review = reviewStorage.getReviewById(reviewId).orElseThrow(() ->
+        Review review = reviewRepository.getReviewById(reviewId).orElseThrow(() ->
                 new NotFoundException("Couldn't add like unexisting review with id = " + reviewId));
-        reviewStorage.addLikeToReview(userId, reviewId);
+        reviewRepository.addLikeToReview(userId, reviewId);
     }
 
     public void addDislikeToReview(int userId, int reviewId) {
         User user = userStorage.findUser(userId).orElseThrow(() ->
                 new NotFoundException("Couldn't add review from unexisting user with id = " + userId));
-        Review review = reviewStorage.getReviewById(reviewId).orElseThrow(() ->
+        Review review = reviewRepository.getReviewById(reviewId).orElseThrow(() ->
                 new NotFoundException("Couldn't add dislike unexisting review with id = " + reviewId));
-        reviewStorage.addDislikeToReview(userId, reviewId);
+        reviewRepository.addDislikeToReview(userId, reviewId);
     }
 
     public void removeLikeDislikeFromReview(int userId, int reviewId) {
         User user = userStorage.findUser(userId).orElseThrow(() ->
                 new NotFoundException("Couldn't add review from unexisting user with id = " + userId));
-        Review review = reviewStorage.getReviewById(reviewId).orElseThrow(() ->
+        Review review = reviewRepository.getReviewById(reviewId).orElseThrow(() ->
                 new NotFoundException("Couldn't remove like/dislike unexisting review with id = " + reviewId));
-        reviewStorage.removeLikeDislikeFromReview(userId, reviewId);
+        reviewRepository.removeLikeDislikeFromReview(userId, reviewId);
     }
 
 }
