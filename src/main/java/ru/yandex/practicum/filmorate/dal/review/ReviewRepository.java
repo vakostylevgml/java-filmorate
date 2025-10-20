554d1ac545ebaeb65fb780592e8ecd4f8f365b90
Vladislav Kostylev <kostylev.v@gmail.com>
2025-10-21 00:58:53 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/dal/review/ReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/review/ReviewRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dal.BaseRepository;
 import ru.yandex.practicum.filmorate.model.Review;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
